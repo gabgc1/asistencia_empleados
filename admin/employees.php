36fdb6808b2e32f9c4fa79d07dc7a0a7216b62fe
Gabriel <ggarincatalan@gmail.com>
2022-11-30 21:55:58 -0600

    mods

--- admin/employees.php
+++ admin/employees.php
@@ -1,4 +1,3 @@
-
 <div class="card">
     <div class="card-header d-flex justify-content-between">
         <h3 class="card-title">Lista de Empleados</h3>
@@ -29,95 +28,94 @@
                 </tr>
             </thead>
             <tbody>
-                <?php 
+                <?php
                 $sql = "SELECT e.*,(e.lastname || ', ' || e.firstname || ' ' || e.middlename) as `name`,d.name as dept, p.name as position FROM `employee_list`e inner join `position_list` p on e.position_id = p.position_id INNER JOIN `department_list` d on e.department_id = d.department_id   order by `name` asc";
                 $qry = $conn->query($sql);
                 $i = 1;
-                    while($row = $qry->fetchArray()):
+                while ($row = $qry->fetchArray()) :
                 ?>
-                <tr>
-                    <td class="text-center p-0"><?php echo $i++; ?></td>
-                    <td class="py-0 px-1"><?php echo $row['employee_code'] ?></td>
-                    <td class="py-0 px-1"><?php echo $row['name'] ?>
-                    <?php if($row['gender'] == "Male"): ?>
-                        <span class="fa fa-mars mx-1 text-primary opacity-50" title="Male"></span>
-                    <?php else: ?>
-                        <span class="fa fa-venus mx-1 text-danger opacity-50" title="Female"></span>
-                    <?php endif; ?>
-                    </td>
-                    <td class="py-0 px-1">
-                        <small><span class="text-muted">Departmento:</span> <?php echo $row['dept'] ?></small><br>
-                        <small><span class="text-muted">Posición:</span> <?php echo $row['position'] ?></small>
-                    </td>
-                    <td class="py-0 px-1">
-                        <small>Email: <?php echo $row['email'] ?></small><br>
-                        <small>Contact: <?php echo $row['contact'] ?></small>
-                    </td>
-                    <td class="py-0 px-1 text-center">
-                    <?php if($row['status'] == 1): ?>
-                        <span class="badge bg-success rounded-pill">Activo</span>
-                    <?php else: ?>
-                        <span class="badge bg-danger rounded-pill">Inactivo</span>
-                    <?php endif; ?>
-                    </td>
-                    <th class="text-center py-0 px-1">
-                        <div class="btn-group" role="group">
-                            <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle btn-sm rounded-0 py-0" data-bs-toggle="dropdown" aria-expanded="false">
-                            Acción
-                            </button>
-                            <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                            <li><a class="dropdown-item view_data" data-id = '<?php echo $row['employee_id'] ?>' href="javascript:void(0)">Ver</a></li>
-                            <li><a class="dropdown-item edit_data" data-id = '<?php echo $row['employee_id'] ?>' href="javascript:void(0)">Editar</a></li>
-                            <li><a class="dropdown-item delete_data" data-id = '<?php echo $row['employee_id'] ?>' data-name = '<?php echo $row['reference_code']." - ".$row['name'] ?>' href="javascript:void(0)">Eliminar</a></li>
-                            </ul>
-                        </div>
-                    </th>
-                </tr>
+                    <tr>
+                        <td class="text-center p-0"><?php echo $i++; ?></td>
+                        <td class="py-0 px-1"><?php echo $row['employee_code'] ?></td>
+                        <td class="py-0 px-1"><?php echo $row['name'] ?>
+                        </td>
+                        <td class="py-0 px-1">
+                            <small><span class="text-muted">Departmento:</span> <?php echo $row['dept'] ?></small><br>
+                            <small><span class="text-muted">Posición:</span> <?php echo $row['position'] ?></small>
+                        </td>
+                        <td class="py-0 px-1">
+                            <small>Email: <?php echo $row['email'] ?></small><br>
+                            <small>Contact: <?php echo $row['contact'] ?></small>
+                        </td>
+                        <td class="py-0 px-1 text-center">
+                            <?php if ($row['status'] == 1) : ?>
+                                <span class="badge bg-success rounded-pill">Activo</span>
+                            <?php else : ?>
+                                <span class="badge bg-danger rounded-pill">Inactivo</span>
+                            <?php endif; ?>
+                        </td>
+                        <th class="text-center py-0 px-1">
+                            <div class="btn-group" role="group">
+                                <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle btn-sm rounded-0 py-0" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Acción
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                                    <li><a class="dropdown-item view_data" data-id='<?php echo $row['employee_id'] ?>' href="javascript:void(0)">Ver</a></li>
+                                    <li><a class="dropdown-item edit_data" data-id='<?php echo $row['employee_id'] ?>' href="javascript:void(0)">Editar</a></li>
+                                    <li><a class="dropdown-item delete_data" data-id='<?php echo $row['employee_id'] ?>' data-name='<?php echo $row['reference_code'] . " - " . $row['name'] ?>' href="javascript:void(0)">Eliminar</a></li>
+                                </ul>
+                            </div>
+                        </th>
+                    </tr>
                 <?php endwhile; ?>
-               
+
             </tbody>
         </table>
     </div>
 </div>
 <script>
-    $(function(){
-        $('#create_new').click(function(){
-            uni_modal('Agregar Nuevo Emplead@',"manage_employee.php",'mid-large')
+    $(function() {
+        $('#create_new').click(function() {
+            uni_modal('Agregar Nuevo Emplead@', "manage_employee.php", 'mid-large')
         })
-        $('.edit_data').click(function(){
-            uni_modal('Editar Detalle de Emplead@',"manage_employee.php?id="+$(this).attr('data-id'),'mid-large')
+        $('.edit_data').click(function() {
+            uni_modal('Editar Detalle de Emplead@', "manage_employee.php?id=" + $(this).attr('data-id'), 'mid-large')
         })
-        $('.view_data').click(function(){
-            uni_modal('EmployeeDetails',"view_employee.php?id="+$(this).attr('data-id'),'mid-large')
+        $('.view_data').click(function() {
+            uni_modal('EmployeeDetails', "view_employee.php?id=" + $(this).attr('data-id'), 'mid-large')
         })
-        $('.delete_data').click(function(){
-            _conf("¿Estás seguro de eliminar a <b>"+$(this).attr('data-name')+"</b> de la lista?",'delete_data',[$(this).attr('data-id')])
+        $('.delete_data').click(function() {
+            _conf("¿Estás seguro de eliminar a <b>" + $(this).attr('data-name') + "</b> de la lista?", 'delete_data', [$(this).attr('data-id')])
         })
         $('table td,table th').addClass('align-middle')
         $('table').dataTable({
-            columnDefs: [
-                { orderable: false, targets:6 }
-            ]
+            columnDefs: [{
+                orderable: false,
+                targets: 6
+            }]
         })
     })
-    function delete_data($id){
-        $('#confirm_modal button').attr('disabled',true)
+
+    function delete_data($id) {
+        $('#confirm_modal button').attr('disabled', true)
         $.ajax({
-            url:'./../Actions.php?a=delete_employee',
-            method:'POST',
-            data:{id:$id},
-            dataType:'JSON',
-            error:err=>{
+            url: './../Actions.php?a=delete_employee',
+            method: 'POST',
+            data: {
+                id: $id
+            },
+            dataType: 'JSON',
+            error: err => {
                 consolre.log(err)
                 alert("Ocurrió un error")
-                $('#confirm_modal button').attr('disabled',false)
+                $('#confirm_modal button').attr('disabled', false)
             },
-            success:function(resp){
-                if(resp.status == 'success'){
+            success: function(resp) {
+                if (resp.status == 'success') {
                     location.reload()
-                }else{
+                } else {
                     alert("Ocurrió un error")
-                    $('#confirm_modal button').attr('disabled',false)
+                    $('#confirm_modal button').attr('disabled', false)
                 }
             }
         })
--- admin/manage_employee.php
+++ admin/manage_employee.php
@@ -1,21 +1,21 @@
 <?php
 require_once("./../DBConnection.php");
-if(isset($_GET['id'])){
-$qry = $conn->query("SELECT * FROM `employee_list` where employee_id = '{$_GET['id']}'");
-    foreach($qry->fetchArray() as $k => $v){
+if (isset($_GET['id'])) {
+    $qry = $conn->query("SELECT * FROM `employee_list` where employee_id = '{$_GET['id']}'");
+    foreach ($qry->fetchArray() as $k => $v) {
         $$k = $v;
     }
 }
 ?>
 <style>
-    #logo-img{
-        width:75px;
-        height:75px;
-        object-fit:scale-down;
-        background : var(--bs-light);
-        object-position:center center;
-        border:1px solid var(--bs-dark);
-        border-radius:50% 50%;
+    #logo-img {
+        width: 75px;
+        height: 75px;
+        object-fit: scale-down;
+        background: var(--bs-light);
+        object-position: center center;
+        border: 1px solid var(--bs-dark);
+        border-radius: 50% 50%;
     }
 </style>
 <div class="container-fluid">
@@ -43,8 +43,8 @@ $qry = $conn->query("SELECT * FROM `employee_list` where employee_id = '{$_GET['
                     <div class="form-group">
                         <label for="gender" class="control-label">Género</label>
                         <select name="gender" id="gender" class="form-select form-select-sm rounded-0">
-                            <option value="Male" <?php echo (isset($gender) && $gender == "Male" ) ? 'selected' : '' ?>>Male</option>
-                            <option value="Female" <?php echo (isset($gender) && $gender == "Female" ) ? 'selected' : '' ?>>Female</option>
+                            <option value="Male" <?php echo (isset($gender) && $gender == "Male") ? 'selected' : '' ?>>Male</option>
+                            <option value="Female" <?php echo (isset($gender) && $gender == "Female") ? 'selected' : '' ?>>Female</option>
                         </select>
                     </div>
                     <div class="form-group">
@@ -62,41 +62,10 @@ $qry = $conn->query("SELECT * FROM `employee_list` where employee_id = '{$_GET['
                         <textarea name="address" id="address" cols="30" rows="3" style="resize:none" class="form-control form-control-sm rounded-0" required><?php echo isset($address) ? $address : '' ?></textarea>
                     </div>
                     <div class="form-group">
-                        <label for="department_id" class="control-label">Departmento</label>
-                        <select name="department_id" id="department_id" class="form-select form-select-sm select2 rounded-0">
-                            <option <?php echo (!isset($department_id)) ? 'selected' : '' ?> disabled>Please Select department</option>
-                            <?php
-                            $department = $conn->query("SELECT * FROM department_list where `status` = 1 ".(isset($department_id) ? " or department_id ='{$department_id}'" : "")." order by `name` asc");
-                            while($row= $department->fetchArray()):
-                            ?>
-                                <option value="<?php echo $row['department_id'] ?>" <?php echo (isset($department_id) && $department_id == $row['department_id'] ) ? 'selected' : '' ?>><?php echo $row['name'] ?></option>
-                            <?php endwhile; ?>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="position_id" class="control-label">Posición</label>
-                        <select name="position_id" id="position_id" class="form-select form-select-sm select2 rounded-0">
-                            <option <?php echo (!isset($position_id)) ? 'selected' : '' ?> disabled>Selecciona Posición</option>
-                            <?php
-                            $position = $conn->query("SELECT * FROM position_list where `status` = 1 ".(isset($position_id) ? " or position_id ='{$position_id}'" : "")." order by `name` asc");
-                            while($row= $position->fetchArray()):
-                            ?>
-                                <option value="<?php echo $row['position_id'] ?>" <?php echo (isset($position_id) && $position_id == $row['position_id'] ) ? 'selected' : '' ?>><?php echo $row['name'] ?></option>
-                            <?php endwhile; ?>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="avatar" class="control-label">Imagen</label>
-                        <input type="file" name="avatar" id="avatar" required class="form-control form-control-sm rounded-0" accept="image/png,image/jpeg" <?php echo !isset($employee_id) ? 'required' : '' ?> onchange="display_img(this)">
-                    </div>
-                    <div class="form-group text-center mt-2">
-                        <img src="<?php echo isset($employee_id) && is_file('./../avatars/'.$employee_id.'.png') ? './../avatars/'.$employee_id.'.png' : './../images/no-image-available.png' ?>" id="logo-img" alt="Avatar">
-                    </div>
-                    <div class="form-group">
                         <label for="status" class="control-label">Estado</label>
                         <select name="status" id="status" class="form-select form-select-sm rounded-0">
-                            <option value="1" <?php echo (isset($status) && $status == 1 ) ? 'selected' : '' ?>>Activo</option>
-                            <option value="0" <?php echo (isset($status) && $status == 0 ) ? 'selected' : '' ?>>Inactivo</option>
+                            <option value="1" <?php echo (isset($status) && $status == 1) ? 'selected' : '' ?>>Activo</option>
+                            <option value="0" <?php echo (isset($status) && $status == 0) ? 'selected' : '' ?>>Inactivo</option>
                         </select>
                     </div>
                 </div>
@@ -106,27 +75,27 @@ $qry = $conn->query("SELECT * FROM `employee_list` where employee_id = '{$_GET['
 </div>
 
 <script>
-    function display_img(input){
+    function display_img(input) {
         if (input.files && input.files[0]) {
-	        var reader = new FileReader();
-	        reader.onload = function (e) {
-	        	$('#logo-img').attr('src', e.target.result);
-	        }
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#logo-img').attr('src', e.target.result);
+            }
 
-	        reader.readAsDataURL(input.files[0]);
-	    }
+            reader.readAsDataURL(input.files[0]);
+        }
     }
-    $(function(){
-        $('#enrollee-form').submit(function(e){
+    $(function() {
+        $('#enrollee-form').submit(function(e) {
             e.preventDefault();
             $('.pop_msg').remove()
             var _this = $(this)
             var _el = $('<div>')
-                _el.addClass('pop_msg')
-            $('#uni_modal button').attr('disabled',true)
+            _el.addClass('pop_msg')
+            $('#uni_modal button').attr('disabled', true)
             $('#uni_modal button[type="submit"]').text('submitting form...')
             $.ajax({
-                url:'./../Actions.php?a=save_employee',
+                url: './../Actions.php?a=save_employee',
                 data: new FormData($(this)[0]),
                 cache: false,
                 contentType: false,
@@ -134,24 +103,24 @@ $qry = $conn->query("SELECT * FROM `employee_list` where employee_id = '{$_GET['
                 method: 'POST',
                 type: 'POST',
                 dataType: 'json',
-                error:err=>{
+                error: err => {
                     console.log(err)
                     _el.addClass('alert alert-danger')
                     _el.text("Ocurrió un error")
                     _this.prepend(_el)
                     _el.show('slow')
-                     $('#uni_modal button').attr('disabled',false)
-                     $('#uni_modal button[type="submit"]').text('Save')
+                    $('#uni_modal button').attr('disabled', false)
+                    $('#uni_modal button[type="submit"]').text('Save')
                 },
-                success:function(resp){
-                    if(resp.status == 'success'){
+                success: function(resp) {
+                    if (resp.status == 'success') {
                         _el.addClass('alert alert-success')
-                        $('#uni_modal').on('hide.bs.modal',function(){
+                        $('#uni_modal').on('hide.bs.modal', function() {
                             location.reload()
                         })
-                        if("<?php echo isset($employee_id) ?>" != 1)
-                        _this.get(0).reset();
-                    }else{
+                        if ("<?php echo isset($employee_id) ?>" != 1)
+                            _this.get(0).reset();
+                    } else {
                         _el.addClass('alert alert-danger')
                     }
                     _el.text(resp.msg)
@@ -159,8 +128,8 @@ $qry = $conn->query("SELECT * FROM `employee_list` where employee_id = '{$_GET['
                     _el.hide()
                     _this.prepend(_el)
                     _el.show('slow')
-                     $('#uni_modal button').attr('disabled',false)
-                     $('#uni_modal button[type="submit"]').text('Save')
+                    $('#uni_modal button').attr('disabled', false)
+                    $('#uni_modal button[type="submit"]').text('Save')
                 }
             })
         })
